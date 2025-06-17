a4ba2ca5c04466091462e8f053e73292af7013dd
xXMomyXx <150335339+xXMomyXx@users.noreply.github.com>
2025-06-17 12:01:28 +0200

    Added support for Cravatta and Material models

--- FinaleAPI/Data/ApiDbContext.cs
+++ FinaleAPI/Data/ApiDbContext.cs
@@ -1,4 +1,4 @@
-﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
 using FinaleAPI.Models;
 
 namespace FinaleAPI.Data
@@ -8,16 +8,22 @@ namespace FinaleAPI.Data
         public DbSet<Article> Articles { get; set; }
         public DbSet<Center> Centers { get; set; }
         public DbSet<ODP> ODPs { get; set; }
+        public DbSet<Material_idsecondary> Materials_idsecondary { get; set; }
+        public DbSet<Material> Materials { get; set; }
+
+
 
         public ApiDbContext()
         {
         }
 
-        public ApiDbContext(DbSet<Article> articles, DbSet<Center> centers, DbSet<ODP> odps)
+        public ApiDbContext(DbSet<Article> articles, DbSet<Center> centers, DbSet<ODP> odps, DbSet<Material_idsecondary> materials_idsecondary, DbSet<Material> materials)
         {
             Articles = articles;
             Centers = centers;
             ODPs = odps;
+            Materials_idsecondary = materials_idsecondary;
+            Materials = materials;
         }
 
         public ApiDbContext(DbContextOptions<ApiDbContext> options) : base(options) { }
@@ -38,6 +44,16 @@ namespace FinaleAPI.Data
             {
                 entity.ToTable("odps_v2");
                 entity.HasKey(e => e.ID_ODP);
+            });
+            modelBuilder.Entity<Material_idsecondary>(entity =>
+            {
+                entity.ToTable("material_idsecondarys");
+                entity.HasNoKey();
+            });
+            modelBuilder.Entity<Material>(entity =>
+            {
+                entity.ToTable("materials");
+                entity.HasKey(e => e.ID_Material);
             });
         }
     }
