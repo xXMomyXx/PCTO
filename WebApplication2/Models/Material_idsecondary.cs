4543bf3684a6f4a98cb68e57b2dbeeac7cb16b6b
xXMomyXx <150335339+xXMomyXx@users.noreply.github.com>
2025-06-17 12:08:18 +0200

    Added Models for Material and Cravatta

---
+++ WebApplication2/Models/Material_idsecondary.cs
@@ -0,0 +1,8 @@
+﻿namespace FinaleAPI.Models
+{
+    public class Material_idsecondary
+    {
+        public string IDSecondary { get; set; }
+        public string ID_Material { get; set; }
+    }
+}
